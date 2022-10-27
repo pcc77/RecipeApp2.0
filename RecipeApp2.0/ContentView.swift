388071b83f2038034f6a1a28c633d2e57660c7f2
Penelope Ceron <pcc77@hotmail.com>
2022-10-27 18:23:18 -0500

    Initial Commit

---
+++ RecipeApp2.0/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RecipeApp2.0
+//
+//  Created by Penelope Ceron on 27/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RecipeApp2.0/RecipeApp2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp2_0App.swift
+//  RecipeApp2.0
+//
+//  Created by Penelope Ceron on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
